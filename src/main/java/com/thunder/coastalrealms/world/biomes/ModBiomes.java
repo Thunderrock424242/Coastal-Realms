524b82c8fdd8065e67c603a8348fa3f905362a1b
Thunderrock424242 <masonjoshua8@gmail.com>
2025-05-01 17:43:33 -0400

    still working on the biomes.

--- src/main/java/com/thunder/coastalrealms/world/biomes/ModBiomes.java
+++ src/main/java/com/thunder/coastalrealms/world/biomes/ModBiomes.java
@@ -1,14 +1,9 @@
 package com.thunder.coastalrealms.world.biomes;
 
-import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.*;
-import net.minecraft.world.level.levelgen.carver.ConfiguredWorldCarver;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 import static com.thunder.coastalrealms.CoastalRealms.MOD_ID;
 
